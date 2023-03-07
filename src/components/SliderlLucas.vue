ccb8e887ddcfa4a8b184ce963696bd59b860f141
Lucasibf5 <118900012+Lucasibf5@users.noreply.github.com>
2023-03-07 20:41:50 +0100

    sliderlucas comienzo

---
+++ src/components/SliderlLucas.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://img.freepik.com/vector-gratis/concepto-tecnologia-backend-fondo-lineas-brillantes_1017-28405.jpg?w=826&t=st=1678041091~exp=1678041691~hmac=e58251cd38fd3c81893cd2f97c65276de1ae5bf7719c2fca4564bce40ae87ff2" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://img.freepik.com/vector-gratis/banner-isometrico-devops-desarrollo-operacion_107791-2943.jpg?w=740&t=st=1678045468~exp=1678046068~hmac=bc58056f74057e6da6d844fa99b5036e1e40cb2d714fd06bc8786feb4c7aef32" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://img.freepik.com/foto-gratis/concepto-transformacion-digital-fondo-microchip-tecnologia-ai_53876-124669.jpg?w=740&t=st=1678100506~exp=1678101106~hmac=3f40b0520d4b50af5faf636e1dd3b1931c23986814fa794fe26149805d5481b8" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+</template>
--- src/views/BackendView.vue
+++ src/views/BackendView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/FooterComponent.vue'
+import SliderLucas from '../components/Sliderlucas.vue'
 </script>
 
 <template>
     <NavBar/>
-    <Footer/>
+    <Footer/>`
+    <SliderLucas/>
 </template>
