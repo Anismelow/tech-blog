1a6e238636ad6bf103c20262a7c947f3918fc723
Anismelow <117463292+Anismelow@users.noreply.github.com>
2023-03-07 20:34:09 +0100

    se termino la pagina de front home

--- src/components/CarrouselComponent.vue
+++ src/components/CarrouselComponent.vue
@@ -1,23 +1,28 @@
 <script setup></script>
 
-<template><div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
+<template>
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/img/frond.webp" class="d-block w-100 h-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/img/back.jpg" class="d-block w-100 h-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/img/logodevops.png" class="d-block w-100 h-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/img/it.jpg" class="d-block w-100 h-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div></template>
+</template>
--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,9 +1,21 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/FooterComponent.vue'
+import Card from '../components/CardComponent.vue'
+import Carrrousel from '../components/CarrouselComponent.vue'
+
 </script>
 
 <template>
-    <NavBar/>
-    <Footer/>
+    <NavBar class="mb-5"/>
+    <Carrrousel class="mb-5 "/>
+
+    <div class="d-flex">
+    <Card class="mb-5"/>
+    <Card class="mb-5"/>
+    <Card class="mb-5"/>
+    <Card class="mb-5"/>
+</div>
+
+    <Footer class="mb-5"/>
 </template>
