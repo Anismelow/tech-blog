da621adeb13833e091366a8c1bebf31e6affe5f6
YorlinPacha <yorlin1517@gmail.com>
2023-03-09 09:06:31 +0100

    Actualizacion de textos de las tarjetas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,22 +11,13 @@ import Carrrousel from '../components/CarrouselComponent.vue'
     <Carrrousel class="mb-5 "/>
 
     
+    
     <div class="cards">
     <!-- : indica que es una variable / se lo he quitado-->
     <!-- : Ingresamos los props directamente en el elemento-->
-    <CardYorlin
-      titulo="Front-Responsive Design vs Adaptative Design ¿ Cuál es Mejor ?"
-      descripcion="descripcion frontend 3Red Hat adquiere compañía que facilita desarrollo de apps basadas en contenedoresCodenvy será adquirida por Red Hat una vez que las instacias reguladoras lo aprueben, covirtiendose en prioridad dentro de la estrategia Openshift.io.
-
-La compañía de soluciones de código abierto, Red Hat informó la adquisición de Codenvy, empresa centrada en herramientas nativas en nube para el desarrollo de apps de nueva generación basadas en contenedores."
-      enlace="https://https://www.siliconweek.com/cloud/red-hat-adquiere-compania-que-facilita-desarrollo-de-apps-basadas-en-contenedores-82856blog.nubecolectiva.com/responsive-design-vs-adaptative-design-cual-es-mejor/"
-      imagen="src/assets/img/2017-05-31-684x505.png"
-    />
-    
-    <CardYorlin titulo="Front-10 Componentes Gratis Para Vue JS en este nuevo año 2023 – Parte 1" descripcion="descripcion frontend 2" enlace="https://blog.nubecolectiva.com/10-componentes-gratis-para-vue-js-parte-1/" imagen="/src/img/imagen-2.png"/>
-    
-       
-    <CardYorlin titulo="Front-Responsive Design vs Adaptative Design ¿ Cuál es Mejor ?" descripcion="descripcion frontend 3" enlace="https://blog.nubecolectiva.com/responsive-design-vs-adaptative-design-cual-es-mejor/" imagen="/src/img/imagen-3.png"/>
+    <CardYorlin titulo="Front-Guía Para Desarrolladores FrontEnd Para Crear Aplicaciones" descripcion="El año 2022 resultó ser un año fantástico para la Inteligencia Artificial. Se lanzaron avances notables en el campo, como GPT-3 de OpenAI y Stable Diffuson AI Model de Stability AI , y las inversiones de capital de riesgo en el campo en 2021 fueron de $ 118 mil millones ..." enlace="https://blog.nubecolectiva.com/guia-para-desarrolladores-frontend-para-crear-aplicaciones-de-ia-en-2023/" imagen="/src/img/imagen-1.png"/>
+    <CardYorlin titulo="Front-10 Componentes Gratis Para Vue JS en este nuevo año 2023 – Parte 1" descripcion="Los componentes de la interfaz de usuario son la parte más integral de una aplicación de software, independientemente de si se trata de una aplicación móvil, web, de escritorio, de realidad aumentada o de realidad virtual. Los elementos de la interfaz de usuario son los componentes ..." enlace="https://blog.nubecolectiva.com/10-componentes-gratis-para-vue-js-parte-1/" imagen="/src/img/imagen-2.png"/>
+    <CardYorlin titulo="Front-Responsive Design vs Adaptative Design ¿ Cuál es Mejor ?" descripcion="Si planeas diseñar un sitio web, debes considerar una gran cantidad de tamaños y resoluciones de pantalla, desde un dispositivo práctico (móvil) hasta la pantalla de una computadora portátil o tableta. Y si tu diseño web no se muestra correctamente en ninguno de los dispositivos ... " enlace="https://blog.nubecolectiva.com/responsive-design-vs-adaptative-design-cual-es-mejor/" imagen="/src/img/imagen-3.png"/>
   </div>
 
 
